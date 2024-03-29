271c1db36d01fc6d5b2ad391eb19e5cb376f5c5a
melisekondrej <ondra.melisek@centrum.cz>
2024-03-29 18:03:23 +0100

    update

--- SemestralniProjekt/Program.cs
+++ SemestralniProjekt/Program.cs
@@ -9,6 +9,11 @@ namespace SemestralniProjekt
         {
             Menu();
             int[] poleInput = userInput();
+            int[] poleRandom = nahodnaCisla();
+            WriteAll(poleInput, "Vaše vybraná čísla jsou: ");
+            WriteAll(poleRandom, "Vylosovaná čísla jsou: ");
+            Console.ReadLine();
+
         }
 
         private static void Menu()
@@ -19,31 +24,42 @@ namespace SemestralniProjekt
             Console.WriteLine("---------------------------");
             Console.WriteLine("Vyberte 6 čísel ( 1 - 49 ).");
             Console.WriteLine("---------------------------");
+            Console.WriteLine("Stiskněte enter pro start");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.ReadKey();
+
         }
 
-        //private static int nahodnaCisla(int nahodna)
-        //{
-        //    int[] poleRandom = new int[6];
+        private static int[] nahodnaCisla()
+        {
+            int[] poleRandom = new int[6];
 
-        //    for (int i = 0; i < poleRandom.Length; i++)
-        //    {
-        //        if (true)
-        //        {
+            Random random = new Random();
 
-        //        }
+            for (int i = 0; i < poleRandom.Length; i++)
+            {
 
+                while (true)
+                {
+                    int cislo = random.Next(1, 49);
 
-        //    }
+                    if (!poleRandom.Contains(cislo))
+                    {
+                        poleRandom[i] = cislo;
+                        break;
+                    }
+                }
+            }
+            return poleRandom;
+        }
 
-
-
-        //}
-
-        private static int[] userInput(string input, int x) 
+        private static int[] userInput()
         {
             int[] poleInput = new int[6];
-            
+            string input;
+            int x;
+
+            Console.WriteLine("Vyberte první číslo.");
 
             for (int i = 0; i < poleInput.Length; i++)
             {
@@ -55,23 +71,52 @@ namespace SemestralniProjekt
                         x = int.Parse(input);
                         if (x > 0 && x < 50)
                         {
-                            Console.WriteLine($"Vaše vybrané {i+1}. číslo je {x}.");
+                            Console.WriteLine($"Vaše vybrané {i + 1}. číslo je {x}.\n");
+                            Console.WriteLine($"Vyberte {i + 2} číslo.");
                             break;
                         }
 
                     }
                     Console.WriteLine("Zadejte správné číslo.");
                 }
+                poleInput[i] = x;
             }
-            return x;
+            Console.Clear();
+            return poleInput;
+            
+        }
+
+        private static void WriteAll(int[] pole, string message)
+        {
+            Console.Write(message);
+            for (int i = 0; i < pole.Length; i++)
+            {
+                Console.Write($"{pole[i]} ");
+
+            }
+            
+        }
+
+        private static void shodnaCisla(int[] poleInput, int[] poleRandom)
+        {
+            for (int i = 0; i < poleInput.Length; i++)
+            {
+
+                
+
+
+            }
+
+
+
         }
     }
 }
-/*askdfjlasdf*/                        
-                    
-                
 
-                
-            
-        
-    
+
+
+
+
+
+
+
